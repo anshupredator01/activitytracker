cc4aba204a5f381812e6dca1ef09022b4a2b99c0
Anshu Kumar Pandey <anshupredator01@gmail.com>
2022-12-23 18:36:18 +0530

    Initial commit

---
+++ app/src/main/java/com/example/activitytracker/Onboarding.kt
@@ -0,0 +1,11 @@
+package com.example.activitytracker
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class Onboarding : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_onboarding)
+    }
+}
